b6682e3dfa4f8a5914c5e4ae8315dcacf32fcbd6
pewkers <jmerrill@crimsonsolace.net>
2019-12-04 11:30:34 -0600

    map page and detailspage minor updates. not finished.

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
-export default function DetailsPage() {
+export default function DetailsPage(props) {
+    const restroom = props.location.state
+
+
     return (
-        <div>
-            <h1>DetailsPage!</h1>
+        <div class="main">
+            <h1>RestPoint: {restroom.location_name} {restroom.id}</h1>
+            <div class="secondary">
+                <img src="{image_url}" alt="{restroom.location_name}" height="250" width="250" />
+                <div class="details">
+                    <p>Location: {restroom.location_name}</p>
+                    <p>Address: {restroom.address}</p>
+                    <p>Description: {restroom.description}</p>
+                </div>
+                <div class="attributes">
+                    <p>Changing Table: {restroom.has_changing_table}</p>
+                    <p>Is accessible: {restroom.is_accessible}</p>
+                    <p>Gender Neutral: {restroom.is_gender_neutral}</p>
+                    <p>Stall Count: {restroom.number_of_stalls}</p>
+                    <p>Family Bathroom: {restroom.is_family_bathroom}</p>
+                </div>    
+            </div>
         </div>
     )
 }
