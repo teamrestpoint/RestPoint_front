d84e9401d5524b4ad96f61a69bbab2d1a978254c
Marcos Castillo <marcoscastillo@MacBook-Pro-2.local>
2019-12-03 18:37:25 -0600

    setup react page routing

---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DetailsPage() {
+    return (
+        <div>
+            <h1>DetailsPage!</h1>
+        </div>
+    )
+}
---
+++ src/pages/ReviewPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReviewPage() {
+    return (
+        <div>
+            <h1>ReviewPage!</h1>
+        </div>
+    )
+}
