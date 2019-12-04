720422e9cce7f2678509dfe423e258cc523bae91
Dan Brown <brown.danny.j@gmail.com>
2019-12-03 19:27:57 -0600

    review branch working

--- src/pages/ReviewPage.js
+++ src/pages/ReviewPage.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { Redirect } from 'react-router'
 
-export default function ReviewPage() {
+class ReviewPage extends Component {
+  state = {
+    redirect: false
+  }
+  
+  handleSubmit(event){
+    event.preventDefault()
+    const reviewObject = {
+   
+    }
+    
+  }
+
+  render() {
+    const { redirect } = this.state
+    if (redirect) {
+      return <Redirect to = "/" />
+    }
+    
     return (
-        <div>
-            <h1>ReviewPage!</h1>
-        </div>
+      <div className="field">
+        <Form onSubmit={this.handleSubmit.bind(this)}>
+
+          {/* This spot is for the 4 star Rating Component */}
+
+          <Form.Group controlId="review">
+            <Form.Label>Review</Form.Label>
+            <Form.Control/>
+          </Form.Group>
+
+          <Form.Group controlId="is_accurate">
+            <Form.Check type="checkbox" label="Is Accurate?" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     )
+  }
 }
+
+export default ReviewPage
