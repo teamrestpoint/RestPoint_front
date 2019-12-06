b15997f359a3fd9f8eaaff185023563bbc1f967d
solidlikeplato <ethanwhaley@gmail.com>
2019-12-06 13:47:19 -0600

    formatted date, changed format to be stars

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import reviewAPI from '../api/ReviewAPI'
+import star from '../chicago_star.svg'
 
 
 const Reviews = ({ restroom }) => {
     const [reviewItems, setReviewItems] = useState(null)
+    const parseDate = (dateTime) => {
+        const date = new Date(dateTime)
+        return(`${date.toDateString()}`)
+    }
+
+    const parseStars = (num) => {
+        let stars = []
+        while (num > 0){
+            stars.push(<img src={star} alt="star" height="42" width="42" />)
+            num--
+        }
+        return stars
+    }
 
     const getFilteredReviews = async () => {
         if (!reviewItems) {
@@ -34,8 +48,9 @@ const Reviews = ({ restroom }) => {
             </Link>
             <div>
                 {reviewItems && reviewItems.map(reviewItem => <li className="list-group-item" key={reviewItem.id}>
-                    <h2>{reviewItem.rating} Stars</h2>
-                    <h4>Posted {reviewItem.created_date}</h4>
+                    <h2>{parseStars(reviewItem.rating)}</h2>
+                    <h4>Posted {parseDate(reviewItem.created_date)}</h4>
+                    
                     <h3>{reviewItem.review_text}</h3>
                 </li>)}
             </div>
